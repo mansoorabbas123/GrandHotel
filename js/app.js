c759e7020e0226b9757a3d6408a00fc79a19bd8d
mansoor abbas <mansoor.abbas651@gmail.com>
2022-01-20 16:27:33 +0500

    completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const toggle = document.querySelector(".toggle");
 const colapse = document.querySelector(".sidebar-colapse");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
 
 toggle.addEventListener("click", () => {
   colapse.classList.toggle("active");
+});
+
+toggle.addEventListener("mouseover", () => {
+  line1.classList.toggle("changeLine");
+  line2.classList.toggle("changeLine");
+  console.log("change line");
 });
